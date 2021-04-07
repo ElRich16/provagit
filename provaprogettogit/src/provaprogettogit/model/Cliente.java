e840a6860a111f62e4b7e20e79535bc0024834e5
UTENTE <UTENTE@192.168.200.121>
2021-04-07 17:25:05 +0200

    2a commit ho fatto la classe cliente

---
+++ provaprogettogit/src/provaprogettogit/model/Cliente.java
@@ -0,0 +1,33 @@
+package provaprogettogit.model;
+
+public class Cliente {
+private int id;
+private String nome;
+private String cognome;
+private int telefono;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public String getCognome() {
+	return cognome;
+}
+public void setCognome(String cognome) {
+	this.cognome = cognome;
+}
+public int getTelefono() {
+	return telefono;
+}
+public void setTelefono(int telefono) {
+	this.telefono = telefono;
+}
+
+}
